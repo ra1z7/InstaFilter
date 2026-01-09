c2430898e96b34789df76e7df6dfb9bdd81e62f9
ra1z <purnamaanrai7@gmail.com>
2026-01-09 17:54:53 +0545

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Purnaman Rai (College) on 09/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
