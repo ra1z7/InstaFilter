be8816c99892cc84ace640a9c4ed18e8ea1797e3
ra1z <purnamaanrai7@gmail.com>
2026-01-18 10:23:47 +0545

    Optimization: now the app doesn't load image everytime user changes filter

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selectedImage: PhotosPickerItem?
+    @State private var inputCIImage: CIImage?
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     @State private var showingFilterOptions = false
@@ -96,25 +97,24 @@ struct ContentView: View {
         Task {
             guard let imageAsData = try await selectedImage?.loadTransferable(type: Data.self) else { return }
             guard let uiImage = UIImage(data: imageAsData) else { return }
-            let ciImage = CIImage(image: uiImage)
-            
-            // Core Image filters have a dedicated inputImage property that lets us send in a CIImage for the filter to work with, but often this is thoroughly broken and will cause your app to crash – it’s much safer to use the filter’s setValue() method with the key kCIInputImageKey.
-            currentFilter.setValue(ciImage, forKey: kCIInputImageKey)
+            inputCIImage = CIImage(image: uiImage)
             applyFilter()
         }
     }
     
     func applyFilter() {
+        // Core Image filters have a dedicated inputImage property that lets us send in a CIImage for the filter to work with, but often this is thoroughly broken and will cause your app to crash – it’s much safer to use the filter’s setValue() method with the key kCIInputImageKey.
+        currentFilter.setValue(inputCIImage, forKey: kCIInputImageKey)
+        
         /*
-         
          Core Image filters:
          - Do not all accept the same inputs
          - Use string keys, not properties
          
          When we assign CIFilter.sepiaTone() to a property, we get an object of the class CIFilter that conforms to protocol CISepiaTone.
          That protocol then exposes the .intensity parameter we’ve been using, but internally it will just map it to a call to setValue(_:forKey:).
-         
          */
+        
         let inputKeys = currentFilter.inputKeys
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) } // kCIInputIntensityKey is another Core Image constant value, and it has the same effect as setting the .intensity parameter of the sepia tone filter. Previously, this was all that the protocol was doing internally anyway, but it did provide valuable extra type safety.
         if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 100, forKey: kCIInputRadiusKey) }
@@ -133,7 +133,7 @@ struct ContentView: View {
     
     func setFilter(_ newFilter: CIFilter) {
         currentFilter = newFilter
-        loadImage()
+        applyFilter()
     }
 }
 
